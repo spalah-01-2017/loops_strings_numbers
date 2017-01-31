e2b5f19acba1ceab0ed194d7d3cf1525774008ab
Aleksey Kuzmenko <kurtzpb@gmail.com>
2017-01-31 13:05:00 +0200

    Some changes

--- HomeTask/string.js
+++ HomeTask/string.js
@@ -1,5 +1,5 @@
 var text = 'Вася купил новый телефон за $54,88';
-var replaced = text.replace(',', '.')
-var parsed = parseFloat(replaced.substr(replaced.indexOf('$') + 1, 5));
+//var replaced = text.replace(',', '.')
+var parsed = parseFloat(text.substr(text.indexOf('$') + 1, 5).replace(',', '.'));
 
 console.log(parsed === 54.88);
